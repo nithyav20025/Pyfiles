a234a522564605e6bf495a89f79c51e85a7a04ef
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-23 18:12:26 +0530

    Basics

--- Input.py
+++ Input.py
@@ -4,4 +4,9 @@ z = input("enter your gender")
 
 print("Hi " + x + ", your age is: " + y + ", your gender is: " + z)
 
+age = int(input("Enter your age: "))
 
+if age > 30:
+    print("Your age is greater than 30.")
+else:
+    print("Your age is 30 or less.")
