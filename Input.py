21e199b258401bdc3262a57f8f539cdf0892252c
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-23 18:02:37 +0530

    Basics

--- Input.py
+++ Input.py
@@ -2,5 +2,6 @@ x = input("enter your name")
 y = input("enter your age")
 z = input("enter your gender")
 
-print("Hi " + x + ", your age and gender is: " + y + " " + z)
+print("Hi " + x + ", your age is: " + y + ", your gender is: " + z)
+
 
