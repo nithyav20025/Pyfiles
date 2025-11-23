cdce690d7542a44e1a86feba9add4581c9de610a
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-23 18:31:17 +0530

    Basics

---
+++ Marks.py
@@ -0,0 +1,11 @@
+x = input("Student name: ")
+y = input("Roll number: ")
+z = input("Marks: ")
+print("Student Name:", x, "Roll number:", y, "Marks:", z)
+
+marks = int(z)
+
+if marks > 35:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
