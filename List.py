9fa6c0d64dcdc4aa2b3096c8bcc9bc8011e4cf59
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-25 02:40:52 +0530

    Basics

---
+++ List.py
@@ -0,0 +1,36 @@
+#List with diffrent category
+
+fav_fruits=["Mango","Apple","Banana","Pineapple"]
+fav_vegetables=["Carrot","Beans","Beetroot","Onion"]
+fav_food=["Dosa","Biriyani","Pizza","Idly"]
+
+print("Fruits:",fav_fruits)
+print("Vegetables:",fav_vegetables)
+print("Food:",fav_food)
+
+#list methods
+
+fav_fruits.append("Orange")
+print("After append",fav_fruits)
+
+fav_vegetables.insert(1,"Cabbage")
+print("After insert",fav_vegetables)
+
+fav_food.remove("Pizza")
+print("After remove",fav_food)
+
+fav_food.sort()
+print("After sort",fav_food)
+
+fav_fruits.pop()
+print("After pop",fav_fruits)
+
+fav_food.reverse()
+print("After reverse",fav_food)
+
+fav_fruits.clear()
+print("After clear",fav_fruits)
+
+print("count",fav_food.count('Idly'))
+
+print("count",len('Carrot'))
