bd646d85b40f4bf164a9224b5b6d3800ca643869
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-24 19:39:53 +0530

    Basics

---
+++ Loop.py
@@ -0,0 +1,16 @@
+#FOR Loop
+
+Names=["vignesh","nithya","nivyan","viniya"]
+
+for name in Names:
+    print(name.upper())
+
+#While Loop
+
+Password='1234'
+Enter_your_password=''
+
+while Password != Enter_your_password:
+    Enter_your_password=input("Enter your correct password: ")
+
+print("Login successful")
