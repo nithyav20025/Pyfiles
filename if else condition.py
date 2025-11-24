2e50df6f52f31b9810d63e13c7ecdf6dcd0470ea
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-24 19:18:53 +0530

    Basics

--- if else condition.py
+++ if else condition.py
@@ -33,16 +33,17 @@ if age>=18:
 else:
     print("You are not eligible to drive")
 
-
+#Conditon using AND
 
 Mark=55
 Attendance=65
-
 
 if Mark>=50 and Attendance<=60:
     print("You are allowed to attend the exam")
 else:
     print("You are not allowed to attend the exam")
+
+#Conditon using OR
 
 recharge_amount=299
 data_pack=1.5
@@ -51,6 +52,8 @@ if recharge_amount>=399 or data_pack>=1:
     print("eligible for free 1GB data")
 else:
     print("not eligible for free data")
+
+#Conditon using AND OR
 
 Bill_amount=1200
 offer_days="saturday"
