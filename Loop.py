75481a294db97ff9bc700f00cd4d5710a67411ac
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-24 20:48:58 +0530

    Basics

--- Loop.py
+++ Loop.py
@@ -14,3 +14,41 @@ while Password != Enter_your_password:
     Enter_your_password=input("Enter your correct password: ")
 
 print("Login successful")
+
+#Break concept
+
+for num in range(10):
+    print(num)
+
+duplicate= [1,2,3,6,4,5,6,7,8,9]
+for dup in duplicate:
+    if dup == 6:
+        break
+    print(dup)
+
+#Continue concept
+
+num=[-4,-3,-2,-1,0,1,2,3,4]
+
+for n in num:
+    if n <0:
+        continue
+    print(n)
+
+#While loop Ex
+
+count=5
+while count > 0:
+    print(f"Countdown: {count}")
+    count-=1
+print("Happy New Year!")
+
+items=[]
+
+while True:
+    item=input("Add item (Type 'done' to proceed): ")
+    if item == "done":
+        break
+    items.append(item)
+print("Items in Cart:",items)
+
