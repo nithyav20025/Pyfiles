346d73ad8237a7f7fb377b7a00adba3bfca511f1
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-25 20:29:40 +0530

    Basics

---
+++ Tuples.py
@@ -0,0 +1,18 @@
+product_details=("Eyeliner", "Black", "1 item", 250, "Add to cart", "Proceed to pay", "Order is confirmed")
+print(product_details)
+
+print(product_details [5])  #to find item in the given number
+
+for item in product_details:  # to loop the given details
+    print(item)
+
+print(len(product_details))  #length of the given details
+
+print(product_details.count("Add to cart")) #count of the given details
+
+print(product_details.index("Proceed to pay")) #to find the position of the given item
+
+
+
+
+
