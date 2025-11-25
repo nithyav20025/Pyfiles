5703b0d1fde3f1e410ff62a13e9abbb5c22c614c
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-25 22:32:35 +0530

    Basics

---
+++ Dictionaries.py
@@ -0,0 +1,13 @@
+Employee = {
+    "Name" : "Vignesh",
+    "Email" : "vicky0220@gmail.com",
+    "Age" : 29,
+    "Gender" : "Male",
+    "Birthday" : "02|08|1996",
+    "Designation" : "Software engineer",
+    "Salary" : 40000,
+
+}
+print(Employee["Birthday"])
+print(Employee.get("Age"))
+
