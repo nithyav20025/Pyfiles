930448783e7ec9e3ce34e1272c03db07c486b931
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-27 20:53:33 +0530

    Basics

--- Constructors.py
+++ Constructors.py
@@ -6,7 +6,7 @@ class Employee:
         self.designation = designation
 
     def display(self):
-        print(f"Employee details: {self.name},{self.age},{self.gender},{self.designation}")
+        print(f"Employee details: Name: {self.name}, Age: {self.age}, Gender: {self.gender}, Designation: {self.designation}")
 
 emp1 = Employee("Vignesh",29,"Male","Developer")
 emp1.display()
