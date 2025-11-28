e13866c1d9e0aef76e6dcdd7869711a33d93f5b0
nithyav20025 <nithyaraja2099@gmail.com>
2025-11-28 19:59:41 +0530

    Basics

---
+++ Polymorphism.py
@@ -0,0 +1,17 @@
+
+class dad:
+
+    def house (self):
+      print("Duplex")
+
+class daughter(dad):
+
+    def factory (self):
+        print("Factory")
+
+    def house (self):
+        print ("Blue house")
+
+d=daughter()
+d.house()
+d.factory()
