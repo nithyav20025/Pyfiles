94e66a0c00199d6eb7e5637fb233d9e426316efa
nithyav20025 <nithyaraja2099@gmail.com>
2025-12-13 18:58:13 +0530

    Basics

---
+++ Pure & Impure function.py
@@ -0,0 +1,12 @@
+#Pure functions:
+
+def add(a, b):
+    return a + b
+
+result = add(3, 4)
+print(result)  # Outputs: 7
+
+def subtract(a, b):
+    return a - b
+result = subtract(10, 4)
+print(result)
